7a74295a14cff6060aece3e1fe987c0f050af243
Rafael Makaha <rafael.makaha@gmail.com>
2019-08-26 12:34:25 -0300

    Changes folder name.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ 01aula/02exer02evolucao/aula1exer2Evolucao2Apaga_RafaelMakaha_16-0142369.sql
@@ -0,0 +1,26 @@
+-- --------               << aula1exer2evolucao2 >>              ------------ --
+--                                                                   --
+--                    SCRIPT DE REMOCAO (DDL)                        --
+--                                                                   --
+-- Data Criacao ..........: 21/08/2019                               --
+-- Autor(es) .............: Rafael Makaha Gomes Ferreira             --
+-- Banco de Dados ........: MySQL                                    --
+-- Base de Dados(nome) ...: aula1exer2evolucao2                               --
+--                                                                   --
+-- Data Ultima Alteracao ..: 21/08/2019                              --
+--                                                                   --
+--                                                                   --
+-- PROJETO => SCRIPT DE APAGAR TABELAS                               --
+--                                                                   --
+-- ----------------------------------------------------------------- --
+
+USE aula1exer2evolucao2;
+
+DROP TABLE compoe;
+DROP TABLE gerencia;
+DROP TABLE VENDA;
+DROP TABLE PRODUTO;
+DROP TABLE AREA;
+DROP TABLE EMPREGADO;
+DROP TABLE GERENTE;
+DROP TABLE telefone;
