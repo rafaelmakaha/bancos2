b3d64cbdef1ee05df8fb93d92ee5ad73d57dbe22
Rafael Makaha <rafael.makaha@gmail.com>
2019-09-10 22:59:54 -0300

    Adds aula1exer2evolucao4 solution.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

---
+++ 01aula/02exer04evolucao/aula1exer2Evolucao4Apaga_RafaelMakaha_16-0142369.sql
@@ -0,0 +1,29 @@
+﻿/*
+-- --------            << aula1exer2Evolucao4 >>        ------------ --
+--                                                                   --
+--                    SCRIPT APAGA (DDL)                             --
+--                                                                   --
+-- Data Criacao ..........: 10/09/2019                               --
+-- Autor(es) .............: Rafael Makaha Gomes Ferreira            --
+-- Banco de Dados ........: MySQL                                    --
+-- Base de Dados(nome) ...: aula1exer2Evolucao4                      --
+--                                                                   --
+-- Data Ultima Alteracao ..: 10/09/2019                              --
+--    + Deletando todas as tabelas uma por vez                       --
+--                                                                   --
+-- PROJETO => 1 Base de Dados                                        --
+--         => 8 Tabelas                                              --
+--                                                                   --
+-- ----------------------------------------------------------------- --
+*/
+
+use aula1exer2Evolucao4;
+
+DROP TABLE possui;
+DROP TABLE telefone;
+DROP TABLE VENDA;
+DROP TABLE PRODUTO;
+DROP TABLE AREA;
+DROP TABLE GERENTE;
+DROP TABLE EMPREGADO;
+DROP TABLE PESSOA;
